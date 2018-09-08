d21f2e6d5cbec9a1acd486bc38683eaac43b8476
lakshmithirupathammapathi <41730938+lakshmithirupathammapathi@users.noreply.github.com>
2018-09-08 10:41:09 +0530

    Create 102.java

---
+++ 102.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n%2==0)
+			System.out.println(n/2);
+		else
+			System.out.println(n);
+		s.close();
+
+}
+}
